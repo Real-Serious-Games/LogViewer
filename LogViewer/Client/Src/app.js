b4e79c55c9cab1b1d58c033f9c6877685c952241
Adam <adam.single@realseriousgames.com>
2015-01-08 11:06:11 +1000

    Initial commit. Basic skeleton of angular app on client side. Server simply logs hello world. Run bower install in Client folder to install components

---
+++ LogViewer/Client/Src/app.js
@@ -0,0 +1,17 @@
+﻿
+//
+// Define the 'app' module.
+//
+angular.module('app', [])
+
+//
+// Application controller.
+//
+.controller('AppCtrl', function AppCtrl($scope) {
+    
+    //
+    // Setup the application data-model.
+    //
+    $scope.dataBindingTest = "Hello World";
+
+})
