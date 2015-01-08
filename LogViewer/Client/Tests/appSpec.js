d25ff9853419b44ff068c936630b19c08548195b
Adam <adam.single@realseriousgames.com>
2015-01-08 14:33:15 +1000

    Added Jasmine  and a SpecRunner as well as placeholder tests to make sure the set up works. Run a simple node server (recommend installing http-server with "npm install -g http-server" and running it from the Client folder with "http-server -a localhost -o") to see the test results.

---
+++ LogViewer/Client/Tests/appSpec.js
@@ -0,0 +1,8 @@
+﻿describe("A Suite", function () {
+    it("contains spec with an expectation to be true and is", function () {
+        expect(true).toBe(true);
+    });
+    it("contains spec with an expectation to be true and isn't", function () {
+        expect(true).toBe(false);
+    });
+});
