84cf55f68a30122f2b9ec10f7aa7185036578c78
Adam <adam.single@realseriousgames.com>
2015-02-09 11:16:40 +1000

    Server: Set up express, socket.io, promised mongo and body-parser with the server. Added functions for receiving connections and disconnections from clients, setting up the update loop from the database when a client calls connectToDB

--- LogViewer/Server/Src/server.js
+++ LogViewer/Server/Src/server.js
@@ -1,1 +1,65 @@
-﻿console.log('Hello World');
+﻿var express = require('express');
+var app = express();
+var socketio = require('socket.io');
+var bodyParser = require('body-parser');
+var pmongo = require('promised-mongo');
+var path = require('path');
+
+var database;
+
+app.use(bodyParser.json());
+
+app.use(
+    '/',
+    express.static(path.join(__dirname, './Client'))
+);
+
+var server = app.listen(process.env.PORT || 3412);
+var io = socketio.listen(server);
+
+var data;
+
+var clients = [];
+
+///
+/// When the socket receives a connection
+///
+io.sockets.on('connection', function (client) {
+    addClient(client);
+    //when the connected client sends a connectToDB call set up the connection
+    client.on('connectToDB', function (host, database, collection) {
+        db = pmongo(host + '/' + database);
+        var collection = db.collection(collection);
+        var cursor = collection.find({}, {}, { tailable: true, timeout: false });
+        cursor.on('data', function (doc) {
+            client.emit('update', doc);
+        });
+    });
+    
+    ///
+    /// Disconnect
+    ///
+    client.on('disconnect', function () {
+        removeClient(client);
+    });
+});
+
+///
+/// Add a new client to the array of clients
+///
+var addClient = function (client) {
+    console.log('New client added: ' + client.toString());
+    clients.push(client);
+};
+
+///
+/// Remove a client from the array of clients
+///
+var removeClient = function (client) {
+    console.log('Attempting to remove client: ' + client.toString());
+    var i = array.indexOf(client);
+    if (i != -1) {
+        console.log('Client was found in the list of clients');
+        array.splice(i, 1);
+    }
+};
