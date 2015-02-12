7cb432af4b10b5cf12d7f96b2f51eee4d5c4c59d
Adam <adam.single@realseriousgames.com>
2015-02-12 11:01:09 +1000

    Client: Stripped back some code that I wasn't ready for yet to get the simplest version working. Had an issue with connecting to the server socket using https instead of http. Can look at acquiring a certificate for using https if needed in the future.

--- LogViewer/Client/Src/app.js
+++ LogViewer/Client/Src/app.js
@@ -10,7 +10,7 @@ angular.module('app', [
 // Create the socket using the socket.io service's factory
 //
 .factory('socket', function (socketFactory) {
-    var mySocket = io.connect('https://localhost:3412'); //this needs to be passed the socket of the server
+    var mySocket = io.connect('http://localhost:3412'); //this needs to be passed the socket of the server
     
     socket = socketFactory({
         ioSocket: mySocket
@@ -26,13 +26,4 @@ angular.module('app', [
     // Setup the application data-model.
     //
     $scope.dataBindingTest = "Hello World";
-    
-    //running log of data pushed from the database
-    $scope.logData = [];
-    
-    //On update push the new data to the logData array
-    socket.on('update', function (data) {
-        $scope.logData.push(data);
-    });
-
 })
