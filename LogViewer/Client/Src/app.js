2a8db91677c29b5cbf952ad0715b405fea37feab
Adam <adam.single@realseriousgames.com>
2015-03-12 15:17:43 +1000

    Client: Visual overhaul and clean up. Gearing up to add some filtering functionality.

--- LogViewer/Client/Src/app.js
+++ LogViewer/Client/Src/app.js
@@ -31,15 +31,36 @@ angular.module('app', [
     
     $scope.selectedLog = null;
     
+    $scope.clearLog = function () {
+        $scope.logData = [];
+        $scope.selectedLog = null;
+    }
 
     $scope.selectLog = function (data) {
         $scope.selectedLog = data;
+    };
+    
+    $scope.truncate = function (txt) {
+        if (!txt) {
+            return [];
+        }
+        var lines = txt.split('\n');
+        if (lines.length == 0) {
+            return [];
+        }
+        var firstLine = lines[0];
+        var maxLineLen = 100;
+        if (firstLine.length > maxLineLen) {
+            firstLine = firstLine.substring(0, maxLineLen);
+        }
+        return firstLine + "...";
     };
     
     ///
     /// The update function from the server
     ///
     socket.on('update', function (data) {
+        console.log('update received from server. New log: ' + data.RenderedMessage.toString());
         $scope.logData.splice(0, 0, data);
         $scope.selectedLog = data;
     })
