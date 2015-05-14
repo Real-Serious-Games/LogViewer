45a5ae361259fc916c40adbe55bc0edab6732efa
Adam Single <AdamSingle@SAGAT.realserious.local>
2015-05-14 13:35:55 +1000

    Client: Set up to use the SocketFactory directly and let it handle connecting to the server.

--- LogViewer/Client/Src/app.js
+++ LogViewer/Client/Src/app.js
@@ -6,22 +6,22 @@ angular.module('app', [
     'btford.socket-io',
 ])
 
-//
-// Create the socket using the socket.io service's factory
-//
-.factory('socket', function (socketFactory) {
-    var mySocket = io.connect('http://localhost:3412'); //this needs to be passed the socket of the server
+// //
+// // Create the socket using the socket.io service's factory
+// //
+// .factory('socket', function (socketFactory) {
+//     var mySocket = io.connect('http://localhost:3412'); //this needs to be passed the socket of the server
     
-    socket = socketFactory({
-        ioSocket: mySocket
-    });
-    return socket;
-})
+//     socket = socketFactory({
+//         ioSocket: mySocket
+//     });
+//     return socket;
+// })
 
 //
 // Application controller.
 //
-.controller('AppCtrl', function AppCtrl($scope, socket) {
+.controller('AppCtrl', function AppCtrl($scope, socketFactory) {
     //
     // Setup the application data-model.
     //
