1500fc1c2dbf4fd220b09800fe0a718507728e17
Adam Single <AdamSingle@SAGAT.realserious.local>
2015-05-18 13:39:30 +1000

    Client: Removed old unnecessary commented out code.

--- LogViewer/Client/Src/app.js
+++ LogViewer/Client/Src/app.js
@@ -6,18 +6,6 @@ angular.module('app', [
     'btford.socket-io',
 ])
 
-// //
-// // Create the socket using the socket.io service's factory
-// //
-// .factory('socket', function (socketFactory) {
-//     var mySocket = io.connect('http://localhost:3412'); //this needs to be passed the socket of the server
-    
-//     socket = socketFactory({
-//         ioSocket: mySocket
-//     });
-//     return socket;
-// })
-
 //
 // Application controller.
 //
